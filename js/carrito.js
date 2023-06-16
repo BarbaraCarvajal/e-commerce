15c3d46a2b30dcbc9c81a8b0ea04864f9895b726
barbaracarvajal <barb.carvajalsaez@gmail.com>
2023-06-15 20:50:35 -0400

    se cambia el fondo de los productos

--- js/carrito.js
+++ js/carrito.js
@@ -1,7 +1,13 @@
 
 const productosEnCarrito = JSON.parse(localStorage.getItem("productos-en-carrito"));
 
+const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
+const contenedorProductos = document.querySelector("#carrito-productos");
+const contenedorCarritoAcciones = document.querySelector("#carrito-acciones")
+const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
+
 if(productosEnCarrito){
+    contenedorCarritoVacio.classList.add("disabled")
     
 
 }else{
