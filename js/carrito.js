7b62f1b6101c8358e8c30a3365238c6281d4dff9
barbaracarvajal <barb.carvajalsaez@gmail.com>
2023-06-17 03:47:29 -0400

    recoerdar hacer responsivo

--- js/carrito.js
+++ js/carrito.js
@@ -1,14 +1,22 @@
 
-const productosEnCarrito = JSON.parse(localStorage.getItem("productos-en-carrito"));
+let productosEnCarrito = localStorage.getItem("productos-en-carrito");
+productosEnCarrito = JSON.parse(productosEnCarrito);
+
 
 const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
 const contenedorCarritoProductos = document.querySelector("#carrito-productos");
 const contenedorCarritoAcciones = document.querySelector("#carrito-acciones")
 const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
 let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
+const botonVaciar = document.querySelector("#carrito-acciones-vaciar");
+const contenedorTotal = document.querySelector("#total");
+const botonComprar = document.querySelector("#carrito-acciones-comprar");
+
+
 
 function cargarProductosCarrito(){
-    if(productosEnCarrito){
+    if(productosEnCarrito && productosEnCarrito.length > 0){
+
         contenedorCarritoVacio.classList.add("disabled");
         contenedorCarritoProductos.classList.remove("disabled");
         contenedorCarritoAcciones.classList.remove("disabled");
@@ -20,10 +28,10 @@ function cargarProductosCarrito(){
             const div = document.createElement("div");
             div.classList.add("carrito-producto");
     
-            div.innerHTML = `
+            div.innerHTML =  `
             <img class="carrito-producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
             <div class="carrito-producto-titulo">
-                <small>Titulo</small>
+                <small>Título</small>
                 <h3>${producto.titulo}</h3>
             </div>
             <div class="carrito-producto-cantidad">
@@ -38,8 +46,8 @@ function cargarProductosCarrito(){
                 <small>Subtotal</small>
                 <p>$${producto.precio * producto.cantidad}</p>
             </div>
-            <button class="carrito-producto-eliminar" id="${producto.id}"><i class="bi bi-trash3"></i></button>
-            `;
+            <button class="carrito-producto-eliminar" id="${producto.id}"><i class="bi bi-trash-fill"></i></button>
+        `;
             contenedorCarritoProductos.append(div);
         });
     
@@ -50,6 +58,7 @@ function cargarProductosCarrito(){
         contenedorCarritoComprado.classList.add("disabled");
     }
     actualizarBotonesEliminar();
+    actualizarTotal();
 }
 
 cargarProductosCarrito();
@@ -62,6 +71,37 @@ function actualizarBotonesEliminar(){
     });
 }
 
-function eliminarDelCarrito(){
+function eliminarDelCarrito(evento){
+    const idBoton = evento.currentTarget.id;
+    const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
     
+    productosEnCarrito.splice(index,1);
+    cargarProductosCarrito();
+
+    localStorage.setItem("productos-en-carrito",JSON.stringify(productosEnCarrito));
+}   
+
+botonVaciar.addEventListener("click", vaciarCarrito);
+function vaciarCarrito(){
+    productosEnCarrito.length = 0;
+    localStorage.setItem("productos-en-carrito",JSON.stringify(productosEnCarrito));
+    cargarProductosCarrito();
+}
+
+function actualizarTotal(){
+    const totalCalculado = productosEnCarrito.reduce((acc, producto) => acc + (producto.precio * producto.cantidad),0);
+    total.innerText = `$${totalCalculado}`;
+}
+
+botonComprar.addEventListener("click", comprarCarrito);
+function comprarCarrito(){
+
+    productosEnCarrito.length = 0;
+    localStorage.setItem("productos-en-carrito",JSON.stringify(productosEnCarrito));
+    
+    contenedorCarritoVacio.classList.add("disabled");
+    contenedorCarritoProductos.classList.add("disabled");
+    contenedorCarritoAcciones.classList.add("disabled");
+    contenedorCarritoComprado.classList.remove("disabled");
+
 }
--- js/main.js
+++ js/main.js
@@ -169,9 +169,12 @@ function actualizarBotonesAgregar(){
 }
 let productosEnCarrito;
 
-const productosEnCarritoLS = JSON.parse(localStorage.getItem("productos-en-carrito"));
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+
+
 if(productosEnCarritoLS){
-     productosEnCarrito = productosEnCarritoLS;
+     productosEnCarrito = JSON.parse(productosEnCarritoLS);
      actualizarNumerito(); 
 }else{
     productosEnCarrito = [];
